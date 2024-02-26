633d29cfb081ea7d9b5524991080e410b0b7101f
vazquezea96 <vazquezeduardo1278@gmail.com>
2024-02-26 14:00:10 -0800

    Project scaffolded, server created, db connected

---
+++ pickleball-connect/src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to Pickleball Connect!</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,44 @@
+/* Require modules
+---------------------------------------------------------- */
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors')
+const path = require('path')
+
+
+/* Require the db connection, models, and seed data
+---------------------------------------------------------- */
+const db = require('./models');
+
+
+/* Require the routes in the controllers folder
+--------------------------------------------------------------- */
+
+
+
+/* Create the Express app
+---------------------------------------------------------- */
+const app = express();
+
+
+/* Middleware (app.use)
+---------------------------------------------------------- */
+// cross origin allowance
+app.use(cors())
+// body parser - used for POST/PUT/PATCH routes:
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+
+/* Mount routes
+---------------------------------------------------------- */
+// This tells our app to look at the `controllers/comments.js` file 
+// to handle all routes that begin with `localhost:3000/api/comments`
+
+
+
+/* Tell the app to listen on the specified port
+---------------------------------------------------------- */
+app.listen(process.env.PORT, function () {
+    console.log('Express is listening to port', process.env.PORT);
+});
